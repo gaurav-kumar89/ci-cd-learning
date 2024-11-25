ef9520b42959e191b4a24d01535e4a969d17442c
gaurav-kumar89 <gaurav.kumar132@infosys.com>
2024-11-25 14:10:49 +0530

    Update life-cycle-hook.component.spec.ts

--- src/app/modules/new-module/life-cycle-hook/life-cycle-hook.component.spec.ts
+++ src/app/modules/new-module/life-cycle-hook/life-cycle-hook.component.spec.ts
@@ -2,7 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LifeCycleHookComponent } from './life-cycle-hook.component';
 
-describe('LifeCycleHookComponent', () => {
+xdescribe('LifeCycleHookComponent', () => {
   let component: LifeCycleHookComponent;
   let fixture: ComponentFixture<LifeCycleHookComponent>;
 
